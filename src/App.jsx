9f2aafa981315e703325f2777740592a2f9c8b55
Thaís Silva <thais.vieraa26@gmail.com>
2024-08-18 00:08:52 -0400

    alterações no componente

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import Task from "./components/TaskComponent";
+import './App.css'
+
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar Programação',
+      completed: false,
+
+  },
+  {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+
+  },
+
+]);
+
+  return (
+     <>
+        <div className="container">
+          <AddTask  />  
+          <Tasks  tasks={tasks} />  
+        </div>
+     </>
+
+  );
+
+};
+
+export default App;
